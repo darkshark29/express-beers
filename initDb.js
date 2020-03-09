7339cc8f0576f15dca7878d97bd3b9ed99f220ba
Maxime <max.cardinal@maxcardi.fr>
2020-03-09 17:01:29 +0100

    Add await function

--- initDb.js
+++ initDb.js
@@ -1,24 +1,28 @@
-console.log('===Adding beers to collection===');
-var MongoClient = require('mongodb').MongoClient;
-var collectionName = 'beers';
-const fs = require('fs');
-const jsonData = require('beers/beers.json');
-var url = process.env.MONGODB_ADDON_URI;
-var dbName = process.env.MONGODB_ADDON_DB || 'beers';
-
-let client;
-try {
-client = await MongoClient.connect(url);
-const db = client.db(dbName);
-await  db.collection(collectionName).drop();
-const collection = db.collection(collectionName);
-let jsonBeers = JSON.parse(jsonData);
-
-jsonBeers.forEach(beer => {
-	console.log('=> Adding ' + beer.name);
-	await collection.insertOne(beer);
-});
-} catch(err) {
-console.log(err.stack);
+async function init (){
+	console.log('===Adding beers to collection===');
+	var MongoClient = require('mongodb').MongoClient;
+	var collectionName = 'beers';
+	const fs = require('fs');
+	const jsonData = require('beers/beers.json');
+	var url = process.env.MONGODB_ADDON_URI;
+	var dbName = process.env.MONGODB_ADDON_DB || 'beers';
+	
+	let client;
+	try {
+		client = await MongoClient.connect(url);
+		const db = client.db(dbName);
+		await  db.collection(collectionName).drop();
+		const collection = db.collection(collectionName);
+		let jsonBeers = JSON.parse(jsonData);
+	
+		jsonBeers.forEach(beer => {
+			console.log('=> Adding ' + beer.name);
+			await collection.insertOne(beer);
+		});
+	} catch(err) {
+		console.log(err.stack);
+	}
+	client.close();
 }
-client.close();
+
+init();
