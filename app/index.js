9a35e9aa3a9ba98fdf8200e49633db675e47a9b6
Maxime <max.cardinal@maxcardi.fr>
2020-03-09 15:34:09 +0100

    add step 5

--- app/index.js
+++ app/index.js
@@ -1,12 +1,51 @@
 var express = require('express');
 var app = express();
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
 
-app.get('/', function (req, res) {
-  console.log('Received request from', req.ip)
-  res.send('Hello World!');
+
+app.get('/beers', async function (req, res) {
+  console.log('Received request for beers from', req.ip);
+  let client;
+  try {  
+    client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    var beerList = await db.collection('beers').find().toArray();
+    res.json(beerList);
+  } catch(err) {
+    console.log(err.stack);
+  }
+  client.close();
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
+app.get('/beer/:beerId', async function (req, res) {
+  console.log(`Received request for ${req.params.beerId} from ${req.ip}`);
+  let client;
+  try {  
+    client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    let beerId = req.params.beerId;
+    let beerList = await db.collection('beers').find({id: beerId}).toArray(); 
+    let beer = beerList[0];
+    console.log(beer);
+    res.json(beer);
+  } catch(err) {
+    console.log(err.stack);
+  }
+  client.close();
+});
+
+
+app.use('/beers/img', express.static('img'));
+app.use('/img', express.static('img'));
+app.use(express.static('public'));
+
+var url = 'mongodb://localhost:27017';
+var dbName = 'test'
+
+
+
+var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Listening at http://%s:%s', host, port);
